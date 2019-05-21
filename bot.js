e97482ccd95b1f0aa543273ef59f4b1010bc6aa5
youme1 <50866254+youme1@users.noreply.github.com>
2019-05-21 14:43:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "529316758524264452) return;
+  if (message.author.id !== "529316758524264452") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
